dc8f60e5e3e8d27e3d308d5bb5f3b6e9ee0ba1e3
Duvan Caicedo <duvamendi2@hotmail.com>
2021-12-20 23:18:58 -0500

    front

--- index.js
+++ index.js
@@ -8,40 +8,6 @@ const {validarToken, admin} = require('./middleware/authjwt')
 const jwt = require('jsonwebtoken');
 const key = 'CLAVEDIFICIL';
 
-const cool = require('cool-ascii-faces');
-const path = require('path');
-// const PORT = 3000; 
-
-
-// express()
-//   .use(express.static(path.join(__dirname, 'public')))
-//   .set('views', path.join(__dirname, 'views'))
-//   .set('view engine', 'ejs')
-// //   .get('/', (req, res) => res.render('./app-the-fifth-code-Frontend/ciclo4_frontend/src/index.js'))
-//   .get('/cool', (req, res) => res.send(cool()))
-//   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-/*
-const proyectoAguas =new ProyectoModel({
-    name:'Proyecto de aguas residuales'
-})
-proyectoAguas.save((err,document)=>{
-    if(err){
-        console.log(err);
-        return;
-    }
-});
-
-const consultaProyectos = async ()=>{
-    return await ProyectoModel.find({});
-}
-
-
-api.get('/proyectos',(request,response)=>{
-    consultaProyectos().then(function(resultado){
-        response.json({ proyectos1 :resultado});
-    })
-});*/
-
 const {ApolloServer} = require('apollo-server-express')
 
 const iniciarServidor =async()=>{
@@ -50,25 +16,9 @@ const iniciarServidor =async()=>{
         {
             typeDefs,
             resolvers
-            // context: ({req}) => {
-            //     const token = req.headers.authorization;
-            //     try{
-            //         const perfil = jwt.verify(token, key)
-            //         if(perfil){
-            //             rol = perfil.role
-            //             return {rol}
-            //         }
-            //     }catch(error){
-            //         console.log(error)
-            //     }
-            //     return{}
-            
         });
     await apollo.start()
     apollo.applyMiddleware({app:api,path:'/consulta'})
-    /*api.use((request, response)=>{
-        response.send('Hola')
-    })*/
     api.use(express.json())
     api.use('/api', authRoute)
     api.get('/api/dashboard', [validarToken, admin], (request, response)=>{
